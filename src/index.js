73141c0c0232a2f58978007c21f6ee5bfb4b9482
Masha Shevchuk <mg.bizarre@gmail.com>
2020-12-19 21:08:25 +0300

    homework 2 finished

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@
 function forEach(array, fn) {
   for (let i = 0; i < array.length; ++i) {
     let item = array[i];
-    fn(item);
+    fn(item, i, array);
   }
 }
 
@@ -23,7 +23,7 @@ function map(array, fn) {
   const newArr = [];
   for (let i = 0; i < array.length; i++) {
     const item = array[i];
-    const itemClone = fn(item);
+    const itemClone = fn(item, i, array);
     newArr.push(itemClone);
   }
 
@@ -36,7 +36,15 @@ function map(array, fn) {
  Напишите аналог встроенного метода reduce для работы с массивами
  Посмотрите как работает reduce и повторите это поведение для массива, который будет передан в параметре array
  */
-function reduce(array, fn, initial) {}
+function reduce(array, fn, initial) {
+  let value = initial || array[0];
+
+  for (let i = initial ? 0 : 1; i < array.length; i++) {
+    let currentValue = array[i];
+    value = fn(value, currentValue, i, array);
+  }
+  return value;
+}
 
 /*
  Задание 4:
@@ -46,7 +54,18 @@ function reduce(array, fn, initial) {}
  Пример:
    upperProps({ name: 'Сергей', lastName: 'Петров' }) вернет ['NAME', 'LASTNAME']
  */
-function upperProps(obj) {}
+function upperProps(obj) {
+  let keys = Object.keys(obj);
+  let arr = [];
+  arr = keys;
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    let item = arr[i];
+    item.toString();
+    newArr.push(item.toUpperCase());
+  }
+  return newArr;
+}
 
 /*
  Задание 5 *:
@@ -54,7 +73,60 @@ function upperProps(obj) {}
  Напишите аналог встроенного метода slice для работы с массивами
  Посмотрите как работает slice и повторите это поведение для массива, который будет передан в параметре array
  */
-function slice(array, from, to) {}
+function slice(array, from, to) {
+  let result = [];
+  let start = from;
+  let end = Math.min(to, array.length);
+
+  if (typeof from !== 'number' && to === 0) {
+    result = [];
+  } else if (typeof from !== 'number' && !to) {
+    result = array;
+  } else if (typeof from !== 'number' && to > 0) {
+    for (let i = 0; i < end; i++) {
+      result.push(array[i]);
+    }
+  } else if (typeof from !== 'number' && to < 0) {
+    for (let i = 0; i < array.length + end; i++) {
+      result.push(array[i]);
+    }
+  } else if (typeof from !== 'number' && typeof from !== 'number') {
+    result = [];
+  } else if (from < 0 && from + array.length < 0 && to < 0) {
+    for (let i = 0; i < array.length + end; i++) {
+      result.push(array[i]);
+    }
+  } else if (from < 0 && to < 0) {
+    for (let i = array.length + start; i < array.length + end; i++) {
+      result.push(array[i]);
+    }
+  } else if (from === 0 && to === 0) {
+    result = [];
+  } else if (from < 0 && from + array.length < 0 && !to) {
+    result = array;
+  } else if (from < 0 && from + array.length < 0) {
+    for (let i = 0; i < end; i++) {
+      result.push(array[i]);
+    }
+  } else if (!to && from < 0) {
+    for (let i = array.length + start; i < array.length; i++) {
+      result.push(array[i]);
+    }
+  } else if (!to && from >= 0) {
+    for (let i = start; i < array.length; i++) {
+      result.push(array[i]);
+    }
+  } else if (to < 0) {
+    for (let i = start; i < array.length + end; i++) {
+      result.push(array[i]);
+    }
+  } else {
+    for (let i = start; i < end; i++) {
+      result.push(array[i]);
+    }
+  }
+  return result;
+}
 
 /*
  Задание 6 *:
@@ -62,6 +134,14 @@ function slice(array, from, to) {}
  Функция принимает объект и должна вернуть Proxy для этого объекта
  Proxy должен перехватывать все попытки записи значений свойств и возводить это значение в квадрат
  */
-function createProxy(obj) {}
+function createProxy(obj) {
+  let proxy = new Proxy(obj, {
+    set(obj, prop, val) {
+      obj[prop] = val * val;
+      return true;
+    },
+  });
+  return proxy;
+}
 
 export { forEach, map, reduce, upperProps, slice, createProxy };
